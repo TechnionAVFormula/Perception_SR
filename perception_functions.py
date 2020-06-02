bcff54f59df362d7a58d7daf010aabc591b318e0
EstherVorkin <57112986+EstherVorkin@users.noreply.github.com>
2020-06-02 17:54:51 +0300

    Update perception_functions.py

--- perception_functions.py
+++ perception_functions.py
@@ -3,7 +3,7 @@ import ast
 import cv2
 import numpy as np
 from PIL import Image
-from detect import get_BB_from_img
+#from detect import get_BB_from_img
 from config import CONFIG
 from config import ConfigEnum
 from PIL import Image, ImageDraw, ImageFont
@@ -18,7 +18,7 @@ else:
 
 def get_cones_from_camera(width, height, pixels):
     # convert from bit representation to RGB + depth format
-    img_RGB = convert_img_bits_to_RGBD(width, height, pixels)
+    img_RGB, img_depth = convert_img_bits_to_RGBD(width, height, pixels)
     # set NN parameters
     weights_path = 'outputs/february-2020-experiments/yolo_baseline/9.weights'
     model_cfg = 'model_cfg/yolo_baseline.cfg'
@@ -29,19 +29,23 @@ def get_cones_from_camera(width, height, pixels):
     # classify detected cone to types
     for BB in BB_list:
         cone_color = predict_cone_color(img_RGB,BB)
-        # cone_depth = predict_cone_depth(img_depth,BB)
+        cone_depth = predict_cone_depth(img_depth,BB)
         # BB = [x,y,h,w,type,depth]
         BB.append(cone_color)
+        BB.append(cone_depth)
     # BB_list = [BB_1,BB_2,...,BB_N]
-
-    img_RGB.close()
-
-    return BB_list
+    return img_depth, BB_list
 
 def convert_img_bits_to_RGBD(width, height, pixels):
     # convert bit format to RGB
     img_RGB = Image.frombytes("RGB", (width, height), pixels, 'raw', 'RGBX', 0,-1)
-    return img_RGB
+    # convert bit format to depth channel
+    CHANNEL_COUNT = 4
+    frames = np.frombuffer(pixels, dtype=np.uint8)
+    deinterleaved = [frames[idx::CHANNEL_COUNT] for idx in range(CHANNEL_COUNT)]
+    img_depth = np.reshape(deinterleaved[3], (width, height))
+
+    return img_RGB, img_depth
 
 def cut_cones_from_img(target_img, BB):
     [x, y, h, w] = BB
@@ -54,41 +58,35 @@ def predict_cone_color(target_img, BB):
     frame = np.array(frame) # convert from PIL to cv
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # General mask
-    low_general = np.array([0, 50, 80])
-    high_general = np.array([179, 255, 255])
-    general_mask = cv2.inRange(hsv_frame, low_general, high_general)
-    general = cv2.bitwise_and(frame, frame, mask=general_mask)
-    # cv2.imshow("General", general)
+    imgB = cv2.cvtColor(hsv_frame, cv2.COLOR_BGR2HSV)
+    imgY = cv2.cvtColor(hsv_frame, cv2.COLOR_BGR2HSV)
 
-    # Yellow color
-    low_yellow = np.array([94, 80, 2])
-    high_yellow = np.array([126, 255, 255])
-    yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
-    yellow = cv2.bitwise_and(general, general, mask=yellow_mask)
-    # cv2.imshow("Yellow", yellow)
+    blueLow = np.array([90, 50, 50])
+    blueHigh = np.array([150, 255, 255])
+    yellowLow = np.array([20, 100, 100])
+    yellowHigh = np.array([40, 255, 255])
 
-    # Blue color
-    low_blue = np.array([5, 50, 50])
-    high_blue = np.array([10, 295, 295])
-    blue_mask = cv2.inRange(hsv_frame, low_blue, high_blue)
-    blue = cv2.bitwise_and(general, general, mask=blue_mask)
-    # cv2.imshow("Blue", blue)
+    maskBlue = cv2.inRange(imgB, blueLow, blueHigh)
+    maskBlue2 = cv2.dilate(maskBlue, np.ones((5, 5), np.uint8))
+    maskBlue3 = cv2.erode(maskBlue2, np.ones((5, 5), np.uint8))
+    maskYellow = cv2.inRange(imgY, yellowLow, yellowHigh)
+    maskYellow2 = cv2.dilate(maskYellow, np.ones((5, 5), np.uint8))
+    maskYellow3 = cv2.erode(maskYellow2, np.ones((5, 5), np.uint8))
+    outputBlue = cv2.bitwise_and(imgB, imgB, mask=maskBlue3) ## TODO change to conv function
+    outputYellow = cv2.bitwise_and(imgY, imgY, mask=maskYellow3) ## TODO change to conv function
 
-    # Orange color
-    low_orange = np.array([20, 190, 20])
-    high_orange = np.array([30, 255, 255])
-    orange_mask = cv2.inRange(hsv_frame, low_orange, high_orange)
-    orange = cv2.bitwise_and(general, general, mask=orange_mask)
-    # cv2.imshow("Orange", orange)
+    tempBlue = cv2.cvtColor(outputBlue, cv2.COLOR_BGR2GRAY)
+    tempYellow = cv2.cvtColor(outputYellow, cv2.COLOR_BGR2GRAY)
+    edgedBlue = cv2.Canny(tempBlue, 30, 300)
+    edgedYellow = cv2.Canny(tempYellow, 30, 300)
+    
 
-    final_frame = cv2.hconcat((frame, yellow, blue, orange))
-    # cv2.imshow("final_frame", final_frame)
+    final_frame = cv2.hconcat((frame, outputYellow, outputBlue))
+    cv2.imshow("final_frame", final_frame)
 
     n_white_pix_yellow = np.sum(yellow_mask == 255)
     n_white_pix_blue = np.sum(blue_mask == 255)
-    n_white_pix_orange = np.sum(orange_mask == 255)
-    n_white_pix = [n_white_pix_yellow, n_white_pix_blue, n_white_pix_orange]
+    n_white_pix = [n_white_pix_yellow, n_white_pix_blue]
     # print('Number of white pixels: Yellow_mask = ', n_white_pix_yellow, " | Blue_mask = ", n_white_pix_blue, " | Orange_mask = ", n_white_pix_orange)
     max_value = max(n_white_pix)
     max_idx = n_white_pix.index(max_value)
@@ -97,8 +95,6 @@ def predict_cone_color(target_img, BB):
         return messages.perception.Yellow
     elif max_idx == 1: # cone is blue
         return messages.perception.Blue
-    else: # cone is orange
-        return messages.perception.Orange
 
 def predict_cone_depth(img_depth, BB):
     frame = img_depth
@@ -106,10 +102,49 @@ def predict_cone_depth(img_depth, BB):
     max_pixel_value = array.max()
     return max_pixel_value
 
+def trasform_img_cones_to_xyz(img_cones, img_depth, h_fov, v_fov, width, height):
+    # get BB in image plain (img_cones) and transform it to xyz coordinates of camera (xyz_cones)
+
+    # choose single representative point in each BB:
+    img_cone_points = []  # list of (x,y,type) BB representative point in img plain
+    for img_cone in img_cones:
+        img_cone_points.append(get_BB_img_point(img_cone))
+
+    # extract xyz coordinates of each cone
+    xyz_cones = []  # list of (X,Y,Z,type) in ENU coordinate system (X - right, Y-forward, Z-upward)
+    for img_cone_point in img_cone_points:
+        #img_depth_px = img_depth.load()
+        img_cone_point_depth = img_depth[img_cone_point[0:2]]  # specific point depth value
+        xyz_cones.append(trasform_img_point_to_xyz(img_cone_point,img_cone_point_depth,h_fov,v_fov,width,height))
+        # insert cone type to xyz_cones:
+        xyz_cones[-1].append(img_cone_point[-1])
+
+    return xyz_cones
+
+def trasform_img_point_to_xyz(img_point, img_depth, h_fov, v_fov, width, height):
+    # extract parameters
+    u = img_point[0]
+    v = img_point[1]
+    alpha_h = (180 - h_fov)/2  # [deg]
+    alpha_v = (180 - v_fov)/2  # [deg]
+    # calculating gammas:
+    gamma_h = alpha_h + (1-u / width) * h_fov  # [deg]
+    gamma_v = alpha_v + (v / height) * v_fov  # [deg]
+    # calculating X,Y,Z in ENU coordinate system (X - right, Y-forward, Z-upward)
+    Y = img_depth
+    X = img_depth / np.tan(gamma_h * np.pi / 180)
+    Z = img_depth / np.tan(gamma_v * np.pi / 180)
+
+    return [X, Y, Z]
+
 def get_BB_img_point(img_cone):
     # return representative point in BB (center of BB at the moment)
-    x, y, h, w, color = img_cone
-    return int(x+w/2), int(y+h/2), color
+    x = img_cone[0]
+    y = img_cone[1]
+    w = img_cone[2]
+    h = img_cone[3]
+    type = img_cone[4]
+    return int(x+w/2), int(y+h/2), type
 
 def draw_results_on_image(img, BB_list, type_map):
 
@@ -125,14 +160,14 @@ def draw_results_on_image(img, BB_list, type_map):
         w = BB_list[i][3]
         raw_type = BB_list[i][4]
         # build parameters
-        color = type_map[raw_type-1]
-        text = f"({i}) {color}"
+        type = type_map[raw_type-1]
+        text = f"({i}) {type}"
         x1 = x0 + w
         y1 = y0 + h
         # draw BB + indicative text
-        draw.rectangle((x0, y0, x1, y1), outline=color)
+        draw.rectangle((x0, y0, x1, y1), outline=type)
         w_text, h_text = font.getsize(text)
-        draw.rectangle((x0, y0-h_text, x0 + w_text, y0), fill=color)
+        draw.rectangle((x0, y0-h_text, x0 + w_text, y0), fill=type)
         draw.text((x0, y0-h_text),text , fill=(0, 0, 0, 128))
 
     return img_with_boxes
